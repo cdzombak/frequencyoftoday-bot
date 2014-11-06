610f6710e9e48f3407f928a12460cca66bfa37a6
Chris Dzombak <chris@chrisdzombak.net>
2014-11-05 21:23:40 -0500

    add generator script

---
+++ frequency.rb
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+# spaces:2
+
+require 'require_relative'
+require 'twitter'
+require_relative 'twitter_init'
+
+frequency = 100 + rand(6000-100)
+tweet = "Today’s frequency is #{frequency} Hz. \nhttp://frequencyof.today/index.html\##{frequency}"
+twitter_client.update(tweet)
