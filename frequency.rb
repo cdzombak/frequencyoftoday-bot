88a5af84386a22d1514c255e1bb5f0481cf0a4ba
Chris Dzombak <chris@chrisdzombak.net>
2014-11-14 12:00:04 -0500

    Max out at 3 kHz

--- frequency.rb
+++ frequency.rb
@@ -5,6 +5,6 @@ require 'require_relative'
 require 'twitter'
 require_relative 'twitter_init'
 
-frequency = 100 + rand(6000-100)
+frequency = 100 + rand(3000-100)
 tweet = "Today’s frequency is #{frequency} Hz. \nhttp://frequencyof.today/index.html?f=#{frequency}"
 twitter_client.update(tweet)
