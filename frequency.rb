21400838281e8e92eb00fdcdfc539b7cd178d04f
Chris Dzombak <chris@chrisdzombak.net>
2014-12-08 19:56:52 -0500

    tweak frequency range

--- frequency.rb
+++ frequency.rb
@@ -5,6 +5,6 @@ require 'require_relative'
 require 'twitter'
 require_relative 'twitter_init'
 
-frequency = 100 + rand(3000-100)
+frequency = 90 + rand(3000-90)
 tweet = "Today’s frequency is #{frequency} Hz. \nhttp://frequencyof.today/index.html?f=#{frequency}"
 twitter_client.update(tweet)
