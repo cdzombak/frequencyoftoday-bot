e20c20fe83bdd03b9ba02dfd5f36558fa487c8cd
Chris Dzombak <chris@chrisdzombak.net>
2018-01-31 22:14:48 -0500

    whitespace

--- frequency.rb
+++ frequency.rb
@@ -9,9 +9,9 @@ numberBetween0And100 = rand(100)
 frequency = 220
 
 if numberBetween0And100 < 60
-    frequency = 90 + rand(400-90)
+  frequency = 90 + rand(400-90)
 else
-    frequency = 300 + rand(4000-300)
+  frequency = 300 + rand(4000-300)
 end
 
 tweet = "Today’s frequency is #{frequency} Hz. \nhttp://frequencyof.today/index.html?f=#{frequency}"
